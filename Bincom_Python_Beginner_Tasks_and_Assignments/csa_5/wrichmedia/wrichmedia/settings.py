b1294af1bc75a8c110600611b67e30c6d2512f5a
WRICHY01 <goddyrichard01@gmail.com>
2026-05-31 16:16:53 +0100

    add .env file

--- Bincom_Python_Beginner_Tasks_and_Assignments/csa_5/wrichmedia/wrichmedia/settings.py
+++ Bincom_Python_Beginner_Tasks_and_Assignments/csa_5/wrichmedia/wrichmedia/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,17 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY", 'REMOVED')
-SECRET_KEY = 'REMOVED'
-
-S_SECRET_KEY = os.environ.get("S_SECRET_KEY", SECRET_KEY)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Application definition
 
@@ -144,3 +144,7 @@ LOGOUT_REDIRECT_URL = '/login'
 LOGIN_URL = '/login'
 
 print("media_root: ", MEDIA_ROOT)
+
+
+import sys
+print("Active secret key: ", SECRET_KEY, file=sys.stderr)
